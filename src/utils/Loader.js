419c92ce43fcdb124aa11a1201f18431a0120c4b
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-06-09 19:48:02 +0100

    add critical-css

--- src/utils/Loader.js
+++ src/utils/Loader.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import '../styles/loader.css';
 
-const Loader = ({ isLoading, error}) => {
+const Loader = ({isLoading, error}) => {
     if (isLoading) {
-        return <div>Loading...</div>
+        return (
+            <div class="spinner">
+                <div class="double-bounce1"></div>
+                <div class="double-bounce2"></div>
+            </div>
+        )
     } else if (error) {
         return <div>Sorry, unable to load the page...</div>
     } else {
